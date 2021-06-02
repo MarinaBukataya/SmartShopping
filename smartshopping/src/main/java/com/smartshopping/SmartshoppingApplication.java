70675900d4a53f9ce8b751cd88b7200bc57a4666
Marina Bukataya <mbukataya@yandex.ru>
2021-06-02 23:22:48 +0300

    fix stuff

--- smartshopping/src/main/java/com/smartshopping/SmartshoppingApplication.java
+++ smartshopping/src/main/java/com/smartshopping/SmartshoppingApplication.java
@@ -3,14 +3,12 @@ package com.smartshopping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jnr.ffi.Struct.int16_t;
 
 @SpringBootApplication
 public class SmartshoppingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartshoppingApplication.class, args);
-		//test
 	}
 
 }
